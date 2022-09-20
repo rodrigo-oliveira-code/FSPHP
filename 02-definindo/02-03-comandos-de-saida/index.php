514cab26c73bd70f86c51242f85df58b15242535
Rodrigo Oliveira <rodrigo.oliveirafln@gmail.com>
2022-09-19 22:35:01 -0300

    Novo commit

--- 02-definindo/02-03-comandos-de-saida/index.php
+++ 02-definindo/02-03-comandos-de-saida/index.php
@@ -40,7 +40,7 @@ print $code;
 //print, diferente do echo, não aceita a vírgula
 /*
  * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
- */
+ ***/
 fullStackPHPClassSession("print_r", __LINE__);
 
 $array = [

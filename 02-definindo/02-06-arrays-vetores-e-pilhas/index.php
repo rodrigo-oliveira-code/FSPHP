58a84349ff7bf2e64b2c6b28e4a7b47c3b17f109
Rodrigo Oliveira <rodrigo.oliveirafln@gmail.com>
2022-09-28 21:53:55 -0300

    ➕ :heavy_plus_sign: Nova funcionalidade para debugar os exemplos #build

--- 02-definindo/02-06-arrays-vetores-e-pilhas/index.php
+++ 02-definindo/02-06-arrays-vetores-e-pilhas/index.php
@@ -7,6 +7,10 @@ fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
  */
 fullStackPHPClassSession("index array", __LINE__);
 
+$arrA = array(1, 2, 3);
+
+
+var_dump($arrA);
 
 /**
  * [ associative array ] "key" => "value"

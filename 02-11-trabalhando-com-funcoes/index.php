38f2899c4543879a466002f1ae812a72664b9240
Rodrigo Oliveira <rodrigo.oliveirafln@gmail.com>
2022-10-27 22:10:11 -0300

    Novas aulas em 27/10/2022

--- 02-11-trabalhando-com-funcoes/index.php
+++ 02-11-trabalhando-com-funcoes/index.php
@@ -7,20 +7,39 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  */
 fullStackPHPClassSession("functions", __LINE__);
 
+require __DIR__ . "/fuctions.php";
+
+var_dump(functionName("PearlJam", "AC/DC", "Alter Bridge"));
+var_dump(functionName("Rodrigo", "Schneider", "Teste"));
+
+var_dump(optionArgs("Rodrigo", "teste", ""));
+var_dump(optionArgs("RodrigoS"));
+var_dump(optionArgs("Digo", "Schneider"));
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
 
+$weight = 83;
+$height = 1.83;
+echo calcImc();
+
+var_dump(calcImc());
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
-
+$pay = payTotal(2501);
+$pay = payTotal(5501);
+$pay = payTotal(501);
+$pay = payTotal(51.22);
+echo $pay;
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump (myTeam("Rodrigo","Danilo","Nani","Kelly","Paulo","Alexandre","Luciana","Wellington"));
--- 02-12-valores-constantes/index.php
+++ 02-12-valores-constantes/index.php
@@ -6,7 +6,32 @@ fullStackPHPClassName("02.12 - Constantes e constantes mágicas");
  * [ constantes ] https://php.net/manual/pt_BR/language.constants.php
  */
 fullStackPHPClassSession("constantes", __LINE__);
+//constantes devem ser declaradas separadas
 
+define("COURSE", "FULLSTACK");
+const AUTHOR = "RODRIGO";
+
+$formation = true;
+if($formation){
+    define("COURSE_TYPE", "Formação");
+} else{
+    define("COURSE_TYPE", "Curso");
+}
+
+echo "<p>COURSE_TYPE COURSE AUTHOR</p>"; //AQUI ELE SÓ EXIBE
+echo "<p>", COURSE_TYPE, ", ", COURSE, ", ", AUTHOR, "</p>"; //aqui tem os parametros
+echo "<p>". COURSE_TYPE. ", ". COURSE. ", ". AUTHOR. "</p>"; //aqui é por concatenação
+
+class Config
+{
+    const USER = "root";
+    const HOST = "localhost";
+
+    public function getArg()
+    {
+
+    }
+}
 
 /*
  * [ constantes mágicas ] https://php.net/manual/pt_BR/language.constants.predefined.php
